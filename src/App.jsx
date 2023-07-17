660293434f0ba77956e713c877a76a48ec1833e3
ThunderFlight <igoreshkin2006@gmail.com>
2023-07-17 23:33:51 +0300

    init: add header and section favorits,aboutCarousel,welcomeBanner and add bookData.json

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import { AboutCarousel } from "./components/aboutCarousel/aboutCarousel";
 import { Favorites } from "./components/favorites/favorites";
 import { Header } from "./components/header/header";
 import { WelcomeBanner } from "./components/welcomeBanner/welcomeBanner";
-
+import booksData from "./data/booksData.json";
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
         <WelcomeBanner />
-        {/* <div className={styles.container}> */}
         <AboutCarousel />
-        <Favorites />
-        {/* </div> */}
+        <Favorites data={booksData} />
       </main>
     </div>
   );
